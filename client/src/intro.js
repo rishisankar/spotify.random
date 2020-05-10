7995af1dca78ce688a08d1351bd7ae0d5f00efc4
rishisankar <rishi.sankar@gmail.com>
2020-05-09 23:57:02 -0700

    Speech bubble and text wrapping

--- client/src/intro.js
+++ client/src/intro.js
@@ -5,6 +5,7 @@ import ReactDOM from "react-dom";
 import Typewriter from 'typewriter-effect';
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
 import catPath from './models/cat.gltf';
+import './intro.css'
 
 class Intro extends Component{
 	state = {
@@ -63,13 +64,16 @@ class TextBox extends Component{
 		this.state = {
 			messageNumber: 0,
 		};
-		this.messages = ["Welcome to spotify.random! press 'space' to continue", 
-					"My name is Insert_Cute_Name, and I'm here to guide you through the site!",
-					"Spotify.random is a place for you to discover new music that matches your unique taste.",
-					"Each song is customized perfectly to you-- no more scouring bad playlists in search for the one.",
-					"To start, choose a genre. Then, select the attributes that set your song apart.",
-					"You'll receive 3 songs that fit your description. If you want to continue your musical journey, just refresh to start the process all over again!",
-					"What do you think? Ready to discover the song of your dreams?"];
+		this.messages = [
+			"<span class=\"text_title\">Welcome to spotify.random!</span><br /> press 'space' to continue", 
+			"My name is Insert_Cute_Name, and I'm here to guide you through the site!",
+			"Spotify.random is a place for you to discover new music that matches your unique taste.",
+			"Each song is customized perfectly to you-- no more scouring bad playlists\
+				 in search for the one.",
+			"To start, choose a genre. Then, select the attributes that set your song apart.",
+			"You'll receive 3 songs that fit your description. If you want to continue\
+				 your musical journey, just refresh to start the process all over again!",
+			"What do you think? Ready to discover the song of your dreams?"];
 
 		this.startIntro = this.startIntro.bind(this)
 		document.addEventListener('keydown',this.startIntro);
@@ -85,17 +89,32 @@ class TextBox extends Component{
 
 	componentDidUpdate(){
 		console.log(this.state.messageNumber);
+		var intervalLength;
+		switch(this.state.messageNumber) {
+			case 2:
+				intervalLength = 6480;
+				break;
+			case 3: 
+				intervalLength = 7200;
+				break;
+			case 5:
+				intervalLength = 10560;
+				break;
+			default:
+				intervalLength = 6000;
+		}
+
 		if(this.state.messageNumber > 0 && this.state.messageNumber < 6)
 		{
-			this.interval = setTimeout(() => this.setState({messageNumber: this.state.messageNumber + 1}),  6000);
+			this.interval = setTimeout(() => this.setState({messageNumber: this.state.messageNumber + 1}),  intervalLength);
 		}
 	}
 
 
 	render(){
 		return(
-			<div style={{display: 'flex', position: 'absolute', justifyContent: 'center', alignItems: 'center'}} >
-				<Typewriter options={{strings: this.messages[this.state.messageNumber], autoStart: true, delay: 40}}/>
+			<div id="speech_bubble" >
+				<Typewriter options={{strings: this.messages[this.state.messageNumber], autoStart: true, delay: 60}}/>
 			</div>
 		);
 	}
