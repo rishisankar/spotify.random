8edd42b017ddd09ef929ef2f0a8740a23104903c
knguyen99 <k.nguyen5464@gmail.com>
2020-05-10 15:06:36 -0700

    update cat.gltf, add more path redirects

--- client/src/visualizer.js
+++ client/src/visualizer.js
@@ -1,22 +1,32 @@
 import React, { Component } from "react";
 import * as THREE from 'three';
 import { Redirect } from "react-router-dom";
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
 
 class Visualizer extends Component{
-	state = {
-		finished: false,
-		returnHome: false
+	constructor(props) {
+		super(props);
+		this.state = {
+			finished: false,
+			returnHome: false
+		}
+	}
+
+	componentDidMount() {
+		//add three js stuff here
 	}
 
 	render() {
 		if(!this.state.finished)
 			return (<h1> This is Visual Scene </h1>);
-		if(this.state.returnHome)
+		else if(this.state.returnHome)
 			return(<Redirect to="/" />);
 		else{
 			return(<Redirect to="/djboard" />);
 		}
 	}
 }
+
+
 
 export default Visualizer;
