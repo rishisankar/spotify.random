7a7f2a1a78b9f66967ca4e10719ba7ffcabc2754
rasha-rahman123 <63566908+rasha-rahman123@users.noreply.github.com>
2020-05-10 02:08:21 -0700

    text speed, speech bubble size, animation/lightning setup

--- client/src/intro.js
+++ client/src/intro.js
@@ -17,28 +17,69 @@ class Intro extends Component{
 	componentDidMount() {
 
 		var scene = new THREE.Scene();
+		var clock = new THREE.Clock();
+		var mixer;
 		var camera = new THREE.PerspectiveCamera( 75, window.innerWidth/window.innerHeight, .1, 1000);
-		var renderer = new THREE.WebGLRenderer();
+		var renderer = new THREE.WebGLRenderer({antialias: true});
 		renderer.setSize(window.innerWidth, window.innerHeight);
 		this.mount.appendChild( renderer.domElement );
 		scene.background = new THREE.Color(0xf5cedd);
-    	camera.position.z = 5;
+    	camera.position.z = 3.5;
+			camera.position.y = 2.9;
 
 		var catloader = new GLTFLoader();
 		catloader.load(catPath,
 			function ( gltf ) {
+
+				//animations
+				mixer = new THREE.AnimationMixer(gltf.scene);
+				var action = mixer.clipAction( gltf.animations[ 0 ] );
+				var action2 = mixer.clipAction( gltf.animations[ 1 ] );
+				action.play();
+				action2.play();
+
+				//add cat model to scene
 				scene.add(gltf.scene);
+
+				//lighting
+				var light = new THREE.DirectionalLight( 0xdfebff, 1 );
+				light.position.set( 10, 50, 100 );
+				light.position.multiplyScalar( 1.3 );
+
+				light.castShadow = true;
+
+				light.shadow.mapSize.width = 1024;
+				light.shadow.mapSize.height = 1024;
+
+				var d = 300;
+
+				light.shadow.camera.left = - d;
+				light.shadow.camera.right = d;
+				light.shadow.camera.top = d;
+				light.shadow.camera.bottom = - d;
+
+				light.shadow.camera.far = 1000;
+
+				scene.add( light );
+
+
 				console.log("added cat");
 			}
 		);
 
 		var animate = function () {
+
 			requestAnimationFrame( animate );
+			var delta = clock.getDelta();
+
+			if ( mixer ) mixer.update( delta );
 
 			renderer.render(scene, camera);
 		};
 
 		animate();
+
+
 	}
 
 
@@ -65,15 +106,16 @@ class TextBox extends Component{
 			messageNumber: 0,
 		};
 		this.messages = [
-			"<span class=\"text_title\">Welcome to spotify.random!</span><br /> press 'space' to continue", 
-			"My name is Insert_Cute_Name, and I'm here to guide you through the site!",
-			"Spotify.random is a place for you to discover new music that matches your unique taste.",
-			"Each song is customized perfectly to you-- no more scouring bad playlists\
+			"<span class=\"text_title\">welcome to spotify.random!</span><br /> press 'space' to continue",
+			"my name is Insert_Cute_Name, and i'm here to guide you through the site!",
+			"spotify.random is a place for you to discover new music that matches your unique taste.",
+			"each song is customized perfectly to you-- no more scouring bad playlists\
 				 in search for the one.",
-			"To start, choose a genre. Then, select the attributes that set your song apart.",
-			"You'll receive 3 songs that fit your description. If you want to continue\
+			"to start, choose a genre. then, select the attributes that set your song apart.",
+			"you'll receive 3 songs that fit your description. if you want to continue\
 				 your musical journey, just refresh to start the process all over again!",
-			"What do you think? Ready to discover the song of your dreams?"];
+			"what do you think? ready to discover the song of your dreams?",
+		"yes or heck yes"];
 
 		this.startIntro = this.startIntro.bind(this)
 		document.addEventListener('keydown',this.startIntro);
@@ -92,14 +134,18 @@ class TextBox extends Component{
 		var intervalLength;
 		switch(this.state.messageNumber) {
 			case 2:
-				intervalLength = 6480;
+				intervalLength = 7580;
 				break;
-			case 3: 
+			case 3:
 				intervalLength = 7200;
 				break;
 			case 5:
-				intervalLength = 10560;
+				intervalLength = 12560;
 				break;
+			case 6:
+					intervalLength = 12560;
+					break;
+
 			default:
 				intervalLength = 6000;
 		}
@@ -113,9 +159,13 @@ class TextBox extends Component{
 
 	render(){
 		return(
+
 			<div id="speech_bubble" >
 				<Typewriter options={{strings: this.messages[this.state.messageNumber], autoStart: true, delay: 60}}/>
 			</div>
+
+
+
 		);
 	}
 }
